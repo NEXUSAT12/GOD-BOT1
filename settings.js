c080b05df1da945974c6f6f8778ca412bba26b5d
NEXUS_AT <113586948+NEXUSAT12@users.noreply.github.com>
2023-09-09 20:07:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.antispam = false //auto kick spammer (true to on, false to off)
 
 
 //if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
-global.keyopenai = "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"
+global.keyopenai = "xxIOQXEuHcwK6uVoVmENT3BlbkFJMqHlml6kkl9W9KpNdtVX"
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
