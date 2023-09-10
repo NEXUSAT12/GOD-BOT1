cceb61c83f147c71848b2a12650a0d875281b7ff
NEXUS_AT <113586948+NEXUSAT12@users.noreply.github.com>
2023-09-10 12:34:47 +0530

    Update ❤ɢᴏᴅʙᴏᴛ❤.js

--- ❤ɢᴏᴅʙᴏᴛ❤.js
+++ ❤ɢᴏᴅʙᴏᴛ❤.js
@@ -5848,7 +5848,7 @@ if (!isPrem) return replyprem(mess.premium)
 if (!text)  return m.reply("Some message to guide the user about providing sticker URL..."); 
 let stickerId = text.split("something")[1]; 
 const response = await fetchJson('https://api.telegram.org/botTOKEN/getStickerSet?name=' + encodeURIComponent(stickerId));
-if (response.result.is_animated) return m.reply("Some error message about unsupported stickers");
+if (response.result.is_animated) return replypcGOD("Some error message about unsupported stickers");
 m.reply('Total stickers: ' + response.result.stickers.length + ' ... '); 
 for (let sticker of response.result.stickers) {
 let stickerData = await fetchJson('API URL' + sticker.file_id);
@@ -5866,25 +5866,34 @@ const { data } = await axios.get(`https://nekos.life/api/v2/fact`)
 return replygcGOD(`${themeemoji} *Fact:* ${data.fact}\n`)   
 }
 break
-case "aimage":
+case 'ai': case 'openai':
 try {
-if (global.keyopenai === '') return replygcGOD("Apikey limit exceeded");
-if (!text) return replygcGOD(`Generate image from AI.\n\nExample:\n${prefix + command} man riding horse`)
+if (global.keyopenai === '') return replygcGOD("Api key limi exceeded");
+if (!q) return replygcGOD(`Chat with AI.\n\nExample:\n${prefix + command} What is coding`)
 const { Configuration, OpenAIApi } = require('openai')
 const configuration = new Configuration({
 apiKey: global.keyopenai,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.createImage({
-prompt: text,
-n: 1,
-size: "512x512",
+const response = await openai.createCompletion({
+model: "text-davinci-003",
+prompt: q,
+temperature: 0.3,
+max_tokens: 2000,
+top_p: 1.0,
+frequency_penalty: 0.0,
+presence_penalty: 0.0,
 });
-//console.log(response.data.data[0].url)
-GODincBOT.sendImage(from, response.data.data[0].url, text, m);
-} catch (err) {
-console.log(err);
-replygcGOD("Sorry, there seems to be an error :"+ err);
+replygcGOD(`${response.data.choices[0].text}`);
+} catch (error) {
+if (error.response) {
+console.log(error.response.status);
+console.log(error.response.data);
+console.log(`${error.response.status}\n\n${error.response.data}`);
+} else {
+console.log(error);
+replygcGOD("Sorry, there seems to be an error :"+ error.message);
+}
 }
 break
 case "aimage": {
