0191b87327ea4b180b572c1fbd811f3f2da4364b
NEXUS_AT <113586948+NEXUSAT12@users.noreply.github.com>
2023-09-14 10:17:04 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,32 +1,22 @@
 require('./settings')
 const fs = require('fs');
 const path = require('path');
-
 // Function to get a random image from the theme directory
 function getRandomThemeImage() {
-  const themeDir = path.join(__dirname, './GODMEDIA'+global.theme);
-
-  if (!fs.existsSync(themeDir)) {
-    throw new Error('Theme directory not found!');
-  }
-
-  const files = fs.readdirSync(themeDir).filter(file => {
-    // Assuming you want jpg images, but you can extend this
-    return path.extname(file) === '.jpg';
-  });
-
-  if (files.length === 0) {
-    throw new Error('No images found in theme directory!');
-  }
-
-  const randomImage = files[Math.floor(Math.random() * files.length)];
-  return path.join(themeDir, randomImage);
+const themeDir = path.join(__dirname, './GODMEDIA'+global.theme);
+if (!fs.existsSync(themeDir)) {
+throw new Error('Theme directory not found!');
 }
-
-// Use the function
-try {
-  const randomImagePath = getRandomThemeImage();
-} catch (error) {
-  console.error('Error:', error.message);
+const files = fs.readdirSync(themeDir).filter(file => {
+// Assuming you want jpg images, but you can extend this
+return path.extname(file) === '.jpg';
+});
+if (files.length === 0) {
+throw new Error('No images found in theme directory!');
 }
+const randomImage = files[Math.floor(Math.random() * files.length)];
+return path.join(themeDir, randomImage);
+}
+const randomImagePath = getRandomThemeImage();
+
 module.export = { randomImagePath }
