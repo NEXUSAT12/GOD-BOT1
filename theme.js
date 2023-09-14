fb9f6235fe539a9b43bc65bcad6b3ff62811705a
NEXUS_AT <113586948+NEXUSAT12@users.noreply.github.com>
2023-09-14 09:53:37 +0530

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,33 @@
+require('./settings.json')
+const fs = require('fs');
+const path = require('path');
+
+// Function to get a random image from the theme directory
+function getRandomThemeImage() {
+  const themeDir = path.join(__dirname, './GODMEDIA'+global.theme);
+
+  if (!fs.existsSync(themeDir)) {
+    throw new Error('Theme directory not found!');
+  }
+
+  const files = fs.readdirSync(themeDir).filter(file => {
+    // Assuming you want jpg images, but you can extend this
+    return path.extname(file) === '.jpg';
+  });
+
+  if (files.length === 0) {
+    throw new Error('No images found in theme directory!');
+  }
+
+  const randomImage = files[Math.floor(Math.random() * files.length)];
+  return path.join(themeDir, randomImage);
+}
+
+// Use the function
+try {
+  const randomImagePath = getRandomThemeImage();
+  console.log('Random Image Path:', randomImagePath);
+} catch (error) {
+  console.error('Error:', error.message);
+}
+module.export = { randomImagePath }
