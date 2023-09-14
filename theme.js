18894731723b01ca0ee5a89909e8235d5a36f199
NEXUS_AT <113586948+NEXUSAT12@users.noreply.github.com>
2023-09-14 10:39:04 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -16,6 +16,18 @@ throw new Error('No images found in theme directory!');
 }
 const randomImage = files[Math.floor(Math.random() * files.length)];
 return path.join(themeDir, randomImage);
+} catch(error) {
+const themedir = fs.readdirSync('./GODMEDIA/'+global.theme);
+if (!fs.existsSync(themeDir)) {
+throw('Theme directory not found!')};
+const files = themedir.filter(file => {
+return path.extname(file) == '.jpg' 
+});
+if (files.length === 0) {
+throw('No images found in theme directory!');
+}
+const randomImage = files[Math.floor(Math.random() * files.length)];
+return path.join(themedir,randomImage);
 }
 const randomImagePath = getRandomThemeImage();
 
